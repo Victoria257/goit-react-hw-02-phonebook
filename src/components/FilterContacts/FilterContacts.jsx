3ddb666e66d527056094b6d12513e1e924270cdd
Viktoria <vik25@i.ua>
2023-03-14 09:43:01 +0100

    corrected the comment

--- src/components/FilterContacts/FilterContacts.jsx
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -1,27 +1,26 @@
-import { Component } from "react";
-import css from './FilterContacts.module.css'
+import PropTypes from 'prop-types';
+import css from './FilterContacts.module.css';
 
-class FilterContacts extends Component {
-    render() {
-        const {filter, onChange} = this.props
-        return (
-            <div className={css.filter}> 
-                <h3>Find contacts by name</h3>
-                <input
-                    className={css.filterInput}
-                type='text'
-                    name='text'
-                    value={filter}
-                    onChange={onChange}
-                title="Name may contain only letters, apostrophe, dash and spaces."
-                required
-            >
-                </input>
-                </div>
-        )
-    }
+const FilterContacts = ({ filter, onChange }) => {
+  return (
+    <div className={css.filter}>
+      <h3>Find contacts by name</h3>
+      <input
+        className={css.filterInput}
+        type="text"
+        name="text"
+        value={filter}
+        onChange={onChange}
+        title="Name may contain only letters, apostrophe, dash and spaces."
+        required
+      ></input>
+    </div>
+  );
+};
 
-}
+export default FilterContacts;
 
-export default FilterContacts
-
+FilterContacts.propTypes = {
+  filter: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
